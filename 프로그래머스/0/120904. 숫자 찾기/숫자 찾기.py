3378d39253974a5dc9965588608b901392565ada
gnorm25 <gnorm42@gmail.com>
2024-05-29 14:08:56 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -0,0 +1,10 @@
+def solution(num, k):
+    num= str(num)
+    k= str(k)
+    answer= num.find(k) 
+    if answer==-1: 
+        return answer
+    else: 
+        return answer+1
+    
+            
