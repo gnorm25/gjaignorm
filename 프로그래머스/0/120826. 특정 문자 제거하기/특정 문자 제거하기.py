9e7640581c72a193ce541c412ce894789f386b3a
gnorm25 <gnorm42@gmail.com>
2024-05-30 14:20:38 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -0,0 +1,13 @@
+def solution(my_string, letter):
+    return ''.join([i for i in my_string if i!=letter])
+    
+    
+    
+#     lisss=list(my_string)
+#     liss2=list(my_string)
+#     for x in lisss:
+#         if x==letter:
+#             liss2.remove(x)
+#     answer = "".join(liss2)
+#     return answer
+
