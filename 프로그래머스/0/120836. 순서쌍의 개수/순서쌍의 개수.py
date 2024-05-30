eb11bd560920d73f2fa6bd5dfc8b186c04f3acd9
gnorm25 <gnorm42@gmail.com>
2024-05-30 15:03:02 +0900

    [level 0] Title: 순서쌍의 개수, Time: 33.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,7 +1,12 @@
 def solution(n):
     
-    x=[i for i in range(1, n+1) if n%i==0]
-    return len(x)
+    x=[i for i in range(1, int(n/2+1)) if n%i==0]
+    return len(x)+1
     
-    
+# def solution(n): 
+#     return len(list(filter(lambda x: n%(x+1)==0, range(0))))
+
+# def solution(n): 
+#     x=[i for i in range(n//2, 0, -1) if n%i==0]
+#     return len(x)+1
         
