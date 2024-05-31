caa1ae523d6f8c03295d474ea868636c3cbe1949
gnorm25 <gnorm42@gmail.com>
2024-05-31 10:37:12 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,9 @@
+def solution(s):
+    if s.isdigit()==True: 
+        if len(s)==4 or len(s)==6: 
+            return True
+        else: 
+            return False
+    else: 
+         return False   
+        
