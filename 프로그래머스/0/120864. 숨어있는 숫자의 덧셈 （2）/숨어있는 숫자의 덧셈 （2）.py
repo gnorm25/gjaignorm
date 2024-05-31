3077576c013d5362a5d6e9c188bcce939f448eca
gnorm25 <gnorm42@gmail.com>
2024-05-31 12:37:11 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,6 +1,3 @@
 def solution(my_string):
-    table=str.maketrans('abcdefghijklmnopqrstuvwxyz', '                          ')
-    my_string= my_string.lower()
-    ekqekq= my_string.translate(table).split(' ')
-    ekqekq1= ' '.join(ekqekq).split()
-    return sum(list(map(int, ekqekq1)))
+    answer = 0
+    return answer
