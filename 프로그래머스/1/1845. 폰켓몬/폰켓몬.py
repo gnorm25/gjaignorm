f632a99c43d2d393e0156520ac4e8b2bdcf4e178
gnorm25 <gnorm42@gmail.com>
2024-06-03 10:00:53 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,16 @@
-def solution(nums):
-    dkswnd = list(dict.fromkeys(nums))
+# def solution(nums):
+#     dkswnd = list(dict.fromkeys(nums))
+    
+#     if len(dkswnd)>=len(nums)//2: 
+#         return len(nums)//2
+#     else: 
+#         return len(dkswnd)
+    
+def solution(nums): 
+    dkswnd= set(nums) 
     
     if len(dkswnd)>=len(nums)//2: 
         return len(nums)//2
     else: 
         return len(dkswnd)
-    
-# def solution(nums): 
     
