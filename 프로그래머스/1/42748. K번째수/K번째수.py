d245ab926bcf485d0b52d115279e9063a525fd6c
gnorm25 <gnorm42@gmail.com>
2024-06-10 09:12:59 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,3 @@
 def solution(array, commands):
-    answer=[]
-    for x, y, z in commands: 
-        tt= array[x-1:y]
-        tt.sort()
-        answer.append(tt[z-1])
+    answer = []
     return answer
