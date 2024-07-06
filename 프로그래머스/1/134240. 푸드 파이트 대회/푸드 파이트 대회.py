e435595921d9668d1d716bd8af62ef2dad1d0922
gnorm25 <gnorm42@gmail.com>
2024-07-06 11:05:29 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -3,7 +3,5 @@ def solution(food):
     for cal in range(1, len(food)): 
         for x in range((food[cal])//2): 
             answer.append(str(cal))
-    answer.append('0')
-    answer2= answer[::-1]
-    del answer2[0]
-    return ''.join(answer+answer2)
+    answer= ''.join(answer)
+    return answer+'0'+answer[::-1]
