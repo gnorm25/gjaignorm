d35f9c2f170fed63974c5ad2c078d87d267b8a6e
gnorm25 <gnorm42@gmail.com>
2024-08-22 18:18:30 +0900

    [level 1] Title: 실패율, Time: 3040.67 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,20 +1,15 @@
-
-
-def solution(N, stages):
-    answer = []
-    retired= {}
-    surv_num= len(stages)
+def solution(N, stages): 
+    answer = [] 
+    retired= {} 
+    surv_num= len(stages) 
     for x in range(1, N+1): 
         if surv_num<=0: 
-            break
-        retired.setdefault(x, stages.count(x)/surv_num)
-        surv_num-=stages.count(x)
-    sorted_dict = sorted(retired.items(), key= lambda item:item[1], reverse= True)
-    for x in sorted_dict: 
-        answer.append(x[0])
+            break 
+        retired.setdefault(x, stages.count(x)/surv_num) 
+        surv_num-=stages.count(x) 
+    so = sorted(retired.items(), key= lambda item:item[1], reverse= True) 
+    for x in so: 
+        answer.append(x[0]) 
     while len(answer)<N: 
-        answer.append(len(answer)+1)
-        
-    
-    
-    return answer
+        answer.append(len(answer)+1) 
+    return answer 
